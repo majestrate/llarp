c5514ce4039b40818f02adb4a9471977816dd968
Jeff Becker <jeff@lokinet.io>
2025-06-19 09:42:20 -0400

    make types non-copy non-move

--- llarp/util/non_blocking.hpp
+++ llarp/util/non_blocking.hpp
@@ -7,13 +7,16 @@ namespace llarp::util
   /// sets nonblocking on a FD.
   struct NonBlocking
   {
-    explicit NonBlocking(int fd);
     explicit NonBlocking(const FD& fd);
     /// get the flags this FD started with.
     int
     old_flags() const;
 
+    NonBlocking(const NonBlocking&) = delete;
+    NonBlocking(NonBlocking&&) = delete;
+
    private:
+    explicit NonBlocking(int fd);
     int m_Flags;
   };
 
