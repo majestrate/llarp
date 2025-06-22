81648a7c5dfbc66f729c8bbfb735694a1a33e726
Jeff Becker <jeff@lokinet.io>
2025-06-22 17:01:22 -0400

    forgot files

---
+++ llarp/iwp/hasher.cpp
@@ -0,0 +1,107 @@
+#include "hasher.hpp"
+#include <sodium/crypto_generichash.h>
+#include <functional>
+#include <llarp/ev/ev.hpp>
+#include <llarp/iwp/message_buffer.hpp>
+#include <llarp/net/sock_addr.hpp>
+#include "llarp/crypto/crypto.hpp"
+
+namespace llarp::iwp
+{
+  void
+  Hasher::run_worker_thread()
+  {
+    llarp::util::SetThreadName("llarp-iwp-work");
+    do
+    {
+      auto maybe = m_VerifyHash.popFrontWithTimeout(1s);
+      if (not maybe and not m_VerifyHash.enabled())
+        return;
+      if (not maybe)
+        continue;
+
+      auto result = VerifyResult{std::move(maybe->from), maybe->msg.msgid(), maybe->Verify()};
+      m_ProcessVerified.pushBack(std::move(result));
+      m_Waker->Trigger();
+    } while (true);
+  }
+
+  void
+  Hasher::start(size_t N_workers, std::shared_ptr<EventLoopWakeup> waker)
+  {
+    if (is_running())
+      return;
+    m_Hasher.start(N_workers, [waker]() { waker->Trigger(); });
+    while (N_workers > 0)
+    {
+      m_Threads.emplace_back([this]() { run_worker_thread(); });
+      N_workers--;
+    }
+    m_Waker = waker;
+  }
+
+  void
+  Hasher::stop()
+  {
+    m_VerifyHash.disable();
+    for (auto& thread : m_Threads)
+      thread.join();
+    m_Threads.clear();
+    m_ProcessVerified.disable();
+    m_Hasher.stop();
+  }
+
+  void
+  Hasher::HashOutboundMessage(const uint8_t* ptr, size_t sz, ShortHash& result)
+  {
+    CryptoManager::instance()->shorthash(result, llarp_buffer_t{ptr, sz});
+  }
+
+  ShortHash&
+  Hasher::GetOutboundMessageHash(HashedMessage& msg)
+  {
+    return msg.msg.m_Digest;
+  }
+
+  std::vector<Hasher::VerifyResult>
+  Hasher::poll_verified()
+  {
+    std::vector<VerifyResult> verified;
+    do
+    {
+      auto maybe = m_ProcessVerified.tryPopFront();
+      if (not maybe)
+        break;
+
+      verified.emplace_back(std::move(*maybe));
+    } while (true);
+    return verified;
+  }
+
+  std::vector<Hasher::HashedMessage>
+  Hasher::poll_hashed()
+  {
+    return m_Hasher.poll_hashed_data();
+  }
+
+  bool
+  Hasher::is_running() const
+  {
+    return not m_Threads.empty();
+  }
+
+  void
+  Hasher::async_verify_hash(const InboundMessage& msg, const SockAddr& from)
+  {
+    m_VerifyHash.pushBack(VerifyRequest{msg, from});
+  }
+
+  void
+  Hasher::async_hash_many(const SockAddr& to, std::vector<OutboundMessage> msgs)
+  {
+    std::vector<HashedMessage> hashed;
+    for (auto& msg : msgs)
+      hashed.emplace_back(HashedMessage{std::move(msg), to});
+    m_Hasher.async_hash_vec(std::move(hashed));
+  }
+}  // namespace llarp::iwp
---
+++ llarp/util/multithreaded_hasher.hpp
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <iterator>
+#include <llarp/util/thread/queue.hpp>
+#include <vector>
+#include <functional>
+
+namespace llarp::util
+{
+  namespace
+  {
+    template <typename Iter_t, typename Value_t>
+    concept is_iterator_for =
+        std::forward_iterator<Iter_t> and std::is_same_v<Value_t, typename Iter_t::value_type>;
+
+    template <typename T>
+    concept is_hashbuffer_compatible = std::move_constructible<T> and std::copy_constructible<T>;
+  };  // namespace
+
+  template <
+      typename T,
+      typename Hash_t,
+      typename HashFunc_t = std::function<void(const uint8_t*, size_t, Hash_t&)>,
+      typename GetHashRef_t = std::function<Hash_t&(T&)>>
+    requires is_hashbuffer_compatible<T> and is_hashbuffer_compatible<Hash_t>
+  struct MultithreadedHasher
+  {
+    struct HashedBuffer
+    {
+      T buf;
+
+      inline const uint8_t*
+      data() const
+      {
+        return buf.data();
+      }
+
+      inline size_t
+      size() const
+      {
+        return buf.size();
+      }
+    };
+
+    void
+    async_hash_vec(std::vector<T> vec)
+    {
+      if (vec.empty())
+        return;
+      for (auto& v : vec)
+        m_IngestData.pushBack(HashedBuffer{std::move(v)});
+      vec.clear();
+    }
+
+    std::vector<T>
+    poll_hashed_data()
+    {
+      std::vector<T> hashed;
+      do
+      {
+        auto maybe = m_HashedData.tryPopFront();
+        if (not maybe)
+          break;
+        hashed.emplace_back(std::move(*maybe));
+      } while (true);
+      return hashed;
+    }
+
+    void
+    start(
+        size_t N_threads, std::function<void(void)> notify_ready = []() {})
+    {
+      if (not m_Threads.empty())
+        return;
+      m_Notify = notify_ready;
+      while (N_threads > 0)
+      {
+        m_Threads.emplace_back([this]() { run_thread_worker(); });
+        N_threads--;
+      }
+    }
+
+    void
+    stop()
+    {
+      m_IngestData.disable();
+      for (auto& thread : m_Threads)
+        thread.join();
+      m_HashedData.disable();
+      m_Threads.clear();
+    }
+
+    explicit MultithreadedHasher(HashFunc_t hash_func, GetHashRef_t get_hash)
+        : m_HashFunc{std::move(hash_func)}, m_GetHashRef{std::move(get_hash)}
+    {}
+
+   private:
+    llarp::thread::Queue<HashedBuffer> m_IngestData{128};
+    llarp::thread::Queue<T> m_HashedData{1024};
+    std::vector<std::thread> m_Threads;
+    const HashFunc_t m_HashFunc;
+    std::function<void(void)> m_Notify;
+    const GetHashRef_t m_GetHashRef;
+
+    void
+    run_thread_worker()
+    {
+      llarp::util::SetThreadName("llarp-hasher");
+      do
+      {
+        auto maybe = m_IngestData.popFrontWithTimeout(1s);
+        if (not maybe and not m_IngestData.enabled())
+          return;
+        if (not maybe)
+          continue;
+        m_HashFunc(maybe->data(), maybe->size(), m_GetHashRef(maybe->buf));
+        m_HashedData.pushBack(std::move(maybe->buf));
+        m_Notify();
+      } while (true);
+    }
+  };
+
+}  // namespace llarp::util
