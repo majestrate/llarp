040c12d8806433a1dfd9d2928828ad766641aa55
Jeff Becker <jeff@lokinet.io>
2025-06-29 12:21:44 -0400

    ttl and fragment offset

--- llarp/net/ip_packet.cpp
+++ llarp/net/ip_packet.cpp
@@ -711,6 +711,8 @@ namespace llarp::net
       reply_pkt.icmp_code() = 0;
       auto* hdr = reply_pkt.Header();
       hdr->id++;
+      hdr->ttl--;
+      hdr->frag_offset = 0;
       reply_pkt.UpdateIPv4Address(ToNet(pkt.dstv4()), ToNet(pkt.srcv4()));
       hdr->check = 0;
       hdr->check = ipchksum(pkt.data(), std::min(size_t{hdr->ihl} * 4, pkt.size()));
